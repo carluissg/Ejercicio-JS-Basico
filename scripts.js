6921e57f3ed4c7f96d87be68a1f2f03bfcb541df
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-02-21 08:16:25 +0100

    cmmit inicial

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const rojo = 178;
+const verde = 49;
+const azul = 74;
+
+generarColores(rojo, verde, azul);
+
+function generarColores(rojo, verde, azul) {
+  console.log("El color es R:" + rojo + ", V:" + verde + ", A:" + azul);
+}
+
+
+
+
